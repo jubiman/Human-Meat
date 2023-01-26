63873696512b3702a77ba9a957737a08312e6be0
Melle Bosboom <jubiman@users.noreply.github.com>
2023-01-26 16:57:03 +0100

    update to version 0.21.28
    fix HarmlessMobsPatch.java

--- src/main/java/com/jubiman/humanflesh/patch/HarmlessMobsPatch.java
+++ src/main/java/com/jubiman/humanflesh/patch/HarmlessMobsPatch.java
@@ -2,16 +2,15 @@ package com.jubiman.humanflesh.patch;
 
 import com.jubiman.humanflesh.mob.HarmlessMobs;
 import necesse.engine.modLoader.annotations.ModMethodPatch;
-import necesse.entity.mobs.Attacker;
 import necesse.entity.mobs.Mob;
-import necesse.entity.mobs.MobHitEvent;
+import necesse.entity.mobs.MobBeforeHitCalculatedEvent;
 import net.bytebuddy.asm.Advice;
 
-@ModMethodPatch(target = Mob.class, name = "isHit", arguments = {MobHitEvent.class, Attacker.class})
+@ModMethodPatch(target = Mob.class, name = "doBeforeHitCalculatedLogic", arguments = {MobBeforeHitCalculatedEvent.class})
 public class HarmlessMobsPatch {
 	@Advice.OnMethodEnter
-	static void onEnter(@Advice.This Mob mob, @Advice.Argument(0) MobHitEvent event, @Advice.Argument(1) Attacker attacker) {
-		if (attacker != null && attacker.getAttackOwner() instanceof HarmlessMobs.Harmless) {
+	static void onEnter(@Advice.This Mob mob, @Advice.Argument(0) MobBeforeHitCalculatedEvent event) {
+		if (event.attacker != null && event.attacker.getAttackOwner() instanceof HarmlessMobs.Harmless) {
 			event.prevent();
 			event.showDamageTip = false;
 			event.playHitSound = false;
